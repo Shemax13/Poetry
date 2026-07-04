ba2ceaecaed78f42683e863de93c8da0d0ea8754
Shemax13 <shemax45@gmail.com>
2026-07-04 11:11:50 +0200

    test: simplest worker that just returns hello world

--- test_deploy.mjs
+++ test_deploy.mjs
@@ -1,14 +1,5 @@
 export default {
   async fetch(request, env) {
-    var url = new URL(request.url);
-    var path = url.pathname;
-    
-    if (path === "/_ping") {
-      return new Response(JSON.stringify({ status: "ok", path: path }), {
-        headers: { "Content-Type": "application/json" }
-      });
-    }
-    
-    return new Response("Not found: " + path, { status: 404 });
+    return new Response("hello world\n");
   }
 };
